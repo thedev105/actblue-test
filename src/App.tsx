0cdfaac5ddd52e5d902f04801da74899564dd759
thedev105 <rjamesworkman@gmail.com>
2025-01-27 18:18:42 -0700

    add typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import skaBand from "./band-json/ska-band.json";
 import kpopBand from "./band-json/kpop-band.json";
 import punkBand from "./band-json/punk-band.json";
--- src/BandForm.tsx
+++ src/BandForm.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function BandForm({ band }) {
   return (
     <div>
